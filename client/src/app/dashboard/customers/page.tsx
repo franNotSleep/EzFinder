f9f0851dd602b5b645f3028bdfb22defea73c514
Francisco Jose <frano04930@gmail.com>
2024-07-02 18:35:19 -0400

    change some users->customers

--- client/src/app/dashboard/customers/page.tsx
+++ client/src/app/dashboard/customers/page.tsx
@@ -13,7 +13,7 @@ import { CustomersFilters } from '@/components/dashboard/customer/customers-filt
 import { CustomersTable } from '@/components/dashboard/customer/customers-table';
 import type { Customer } from '@/components/dashboard/customer/customers-table';
 
-export const metadata = { title: `Customers | Dashboard | ${config.site.name}` } satisfies Metadata;
+export const metadata = { title: `Users | Dashboard | ${config.site.name}` } satisfies Metadata;
 
 const customers = [
   {
@@ -113,13 +113,13 @@ export default function Page(): React.JSX.Element {
   const page = 0;
   const rowsPerPage = 5;
 
-  const paginatedCustomers = applyPagination(customers, page, rowsPerPage);
+  const paginatedUsers = applyPagination(customers, page, rowsPerPage);
 
   return (
     <Stack spacing={3}>
       <Stack direction="row" spacing={3}>
         <Stack spacing={1} sx={{ flex: '1 1 auto' }}>
-          <Typography variant="h4">Customers</Typography>
+          <Typography variant="h4">Users</Typography>
           <Stack direction="row" spacing={1} sx={{ alignItems: 'center' }}>
             <Button color="inherit" startIcon={<UploadIcon fontSize="var(--icon-fontSize-md)" />}>
               Import
@@ -137,9 +137,9 @@ export default function Page(): React.JSX.Element {
       </Stack>
       <CustomersFilters />
       <CustomersTable
-        count={paginatedCustomers.length}
+        count={paginatedUsers.length}
         page={page}
-        rows={paginatedCustomers}
+        rows={paginatedUsers}
         rowsPerPage={rowsPerPage}
       />
     </Stack>
